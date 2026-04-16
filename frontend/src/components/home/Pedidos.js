193502536f1b5ec4072fb66f57af0a7624f3a7d1
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-04-16 13:42:02 -0300

    Adicionei a section de pedidos

---
+++ frontend/src/components/home/Pedidos.js
@@ -0,0 +1,29 @@
+function Pedidos() {
+  return (
+    <section className="pedidos">
+      <h2>Pedidos</h2>
+      <div className="pedidos">
+        <div className="pedido">
+          <div className="footer">
+            <h3>Mesa 1</h3>
+            <i class="fa-solid fa-eye fa-lg"></i>
+          </div>
+        </div>
+        <div className="prato">
+          <div className="footer">
+            <h3>Mesa 2</h3>
+            <i class="fa-solid fa-eye fa-lg"></i>
+          </div>
+        </div>
+        <div className="prato">
+          <div className="footer">
+            <h3>Mesa 3</h3>
+            <i class="fa-solid fa-eye fa-lg"></i>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Pedidos
---
+++ frontend/src/components/home/Pratos.js
@@ -0,0 +1,32 @@
+function Pratos() {
+  return (
+    <section className="pratos">
+      <h2>Pratos disponíveis</h2>
+      <div className="pratos">
+        <div className="prato">
+          <img src="https://img.freepik.com/fotos-gratis/closeup-de-carne-assada-com-molho-legumes-e-batatas-fritas-em-um-prato-sobre-a-mesa_181624-35847.jpg?semt=ais_hybrid&w=740&q=80" />
+          <div className="footer">
+            <h3>Arroz e feijão</h3>
+            <i class="fa-solid fa-eye fa-lg"></i>
+          </div>
+        </div>
+        <div className="prato">
+          <img src="https://img.freepik.com/fotos-gratis/closeup-de-carne-assada-com-molho-legumes-e-batatas-fritas-em-um-prato-sobre-a-mesa_181624-35847.jpg?semt=ais_hybrid&w=740&q=80" />
+          <div className="footer">
+            <h3>Arroz e feijão</h3>
+            <i class="fa-solid fa-eye fa-lg"></i>
+          </div>
+        </div>
+        <div className="prato">
+          <img src="https://img.freepik.com/fotos-gratis/closeup-de-carne-assada-com-molho-legumes-e-batatas-fritas-em-um-prato-sobre-a-mesa_181624-35847.jpg?semt=ais_hybrid&w=740&q=80" />
+          <div className="footer">
+            <h3>Arroz e feijão</h3>
+            <i class="fa-solid fa-eye fa-lg"></i>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Pratos
