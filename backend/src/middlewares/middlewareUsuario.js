68e2bf11633e14061256d423bc1f7576013aaab3
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-04-16 07:54:18 -0300

    Adicionei as respostas http

--- backend/src/middlewares/middlewareUsuario.js
+++ backend/src/middlewares/middlewareUsuario.js
@@ -5,15 +5,15 @@ import "dotenv/config"
 export async function checarLogin(request, response, next) {
   const informacaoToken = request.headers["authorization"]
   if(!informacaoToken) {
-    return response.json({erro: "Usuário precisa estar logado para acessar essa rota!"})
+    return response.status(401).json({erro: "Usuário precisa estar logado para acessar essa rota!"})
   }
   const token = informacaoToken.split(" ")[1]
     if(!token) {
-    return response.json({erro: "Token inválido!"})
+    return response.status(401).json({erro: "Token inválido!"})
   }
   jwt.verify(token, process.env.SENHA_JWT, (erro, usuario)=>{
     if(erro){
-      response.json({erro: "Erro ao verificar o token!"})
+      response.status(500).json({erro: "Erro ao verificar o token!"})
     }
     request.user = usuario
     next()
